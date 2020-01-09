d4e9be0291dedb96a7065c744117d95826fe782a
Trayand <patradyn@gmail.com>
2020-01-09 20:16:32 +0700

    complete initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,21 @@
 const router = require('express').Router()
 const Controller;
 
+// for axios in controllers
+// let axiosAPI = require('../helpers/axios') //taruh di atas file controller
+
+// cara pakai 
+// misal instance Controller
+
+// class Controller {
+//     static get teams(req,res,next){
+//         axiosAPI(url)
+//     }
+// }
+
+// isi url tersebut mengarah ke api yang di tuju, semisal: teams, players
+// headers sudah terdaftar sebagai token football-data dan method sebagai get
+
 // router.use('/', Controller)
 
 module.exports = router
