d324805b416adb6dc2b284358d1cf26a01ff80c5
Bayu Oktari <bayu.oktari@gmail.com>
2020-01-10 13:34:54 +0700

    Login Features Done

--- client/main.js
+++ client/main.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-        });
-    }
-    function onSignIn(googleUser) {
-        const id_token = googleUser.getAuthResponse().id_token;
-        $.ajax({
-            type: "post",
-            url: "//localhost:3000/",
-            data: "data",
-            dataType: "dataType",
-            success: function (response) {
-
-            }
-        });
-    }
+    $('.prof-username').hide();
+    $('.after-login').hide()
 });
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        localStorage.removeItem("token")
+        localStorage.removeItem("name")
+        localStorage.removeItem("picture")
+        $('.login-page').show()
+        $('.prof-username').hide();
+
+    });
+}
+function onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    const id_token = googleUser.getAuthResponse().id_token;
+    axios.post('http://localhost:3000/login', {
+        data: {
+            id_token
+        }
+    })
+        .then(payload => {
+            let data = payload.data
+            if (payload.status == 200) {
+                localStorage.setItem("token", data.token)
+                $('.login-page').hide()
+                $('.prof-username').show();
+                $('.prof-username').text(data.name);
+                $('.prof-username').prepend(` <img src="${data.picture}" class="rounded - circle prof - pict" style="width: 30px; "> `);
+                // $(".prof-pict").attr("src", data.picture);
+                $('.after-login').show()
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+
